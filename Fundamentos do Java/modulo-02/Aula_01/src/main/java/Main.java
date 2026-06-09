54abac2979a7246e5af6ba1ee23db50b4c82030b
riandziuba <riandziuba@gmail.com>
2026-06-09 20:17:54 -0300

    feat: add example with gradle

---
+++ Fundamentos do Java/modulo-02/Aula_01/src/main/java/Main.java
@@ -0,0 +1,17 @@
+import com.google.gson.Gson;
+import rdziuba.dev.menu.MenuItem;
+
+import java.math.BigDecimal;
+
+public class Main {
+    static void main(String[] args) {
+        MenuItem chavesDrink = new MenuItem(1L, "Refresco do Chaves", """
+                Suco de Limão, que parece tamarindo, mas tem gosto de groselha
+                """, MenuItem.MenuCategory.DRINKS, new BigDecimal("2.99"), null);
+
+        Gson gson = new Gson();
+        String json = gson.toJson(chavesDrink);
+
+        System.out.println(json);
+    }
+}

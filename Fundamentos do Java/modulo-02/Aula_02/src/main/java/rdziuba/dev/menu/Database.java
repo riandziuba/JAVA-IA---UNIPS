94dc71e9379f3f24e7aa1e85bf4b4dc9bfcd19a1
riandziuba <riandziuba@gmail.com>
2026-06-10 21:27:01 -0300

    feat: dona florinda functions requested

--- Fundamentos do Java/modulo-02/Aula_02/src/main/java/rdziuba/dev/menu/Database.java
+++ Fundamentos do Java/modulo-02/Aula_02/src/main/java/rdziuba/dev/menu/Database.java
@@ -1,30 +1,61 @@
 package rdziuba.dev.menu;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 public class Database {
 
-    public List<MenuItem> getMenuItems() {
-        List<MenuItem> items = new ArrayList<>();
+    private final List<MenuItem> database;
+    private final Map<MenuItem, BigDecimal> priceHistory = new IdentityHashMap<>();
+
+    public Database() {
+        this.database = new ArrayList<>();
 
         MenuItem chavesDrink = new MenuItem(1L, "Refresco do Chaves", """
                 Suco de Limão, que parece tamarindo, mas tem gosto de groselha
                 """, MenuItem.MenuCategory.DRINKS, new BigDecimal("2.99"), null);
-        items.add(chavesDrink);
+        database.add(chavesDrink);
         MenuItem chavesSandwich = new MenuItem(2L, "Sanduiche de presunto do Chaves", "Sanduiche de presunto simples, mas feito com muito amor", MenuItem.MenuCategory.MAIN_COURSE, new BigDecimal("3.50"), new BigDecimal("2.99"));
-        items.add(chavesSandwich);
+        database.add(chavesSandwich);
         MenuItem donaFlorindaPie = new MenuItem(5L, "Torta de Frango da Dona Florinda", "Torta de frnago com recheio cremoso e massa crocante", MenuItem.MenuCategory.MAIN_COURSE, new BigDecimal("12.99"), new BigDecimal("10.99"));
-        items.add(donaFlorindaPie);
+        database.add(donaFlorindaPie);
+    }
 
-        return items;
+    public List<MenuItem> getMenuItems() {
+        return new ArrayList<>(this.database);
     }
 
     public Optional<MenuItem> getById(Long id) {
-        List<MenuItem> menuItems = this.getMenuItems();
-        return menuItems.stream().filter(menuItem -> menuItem.id().equals(id)).findFirst();
+        return this.database.stream().filter(menuItem -> menuItem.id().equals(id)).findFirst();
+    }
+
+    public boolean removeById(Long id) {
+        try {
+            Optional<MenuItem> menuItemOptional = this.getById(id);
+            if (menuItemOptional.isEmpty()) return false;
+            this.database.remove(menuItemOptional.get());
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public boolean updatePriceById(Long id, BigDecimal newPrice) {
+            Optional<MenuItem> menuItemOptional = this.getById(id);
+            if (menuItemOptional.isEmpty()) return false;
+            MenuItem menuItem = menuItemOptional.get();
+            this.database.remove(menuItem);
+            MenuItem newMenuItem = new MenuItem(menuItem.id(), menuItem.name(), menuItem.description(), menuItem.category(), newPrice, menuItem.priceWithDiscount());
+            this.database.add(newMenuItem);
+            priceHistory.put(menuItem, newPrice);
+            return true;
+    }
+
+    public void printPriceHistory() {
+        System.out.println("Histórico de preços:");
+        this.priceHistory.forEach((item, price) -> {
+            System.out.printf("%s: %s => %s \n", item.name(), item.price(), price);
+        });
     }
 
 }
--- Fundamentos do Java/modulo-02/Aula_02/src/main/java/rdziuba/dev/menu/Main.java
+++ Fundamentos do Java/modulo-02/Aula_02/src/main/java/rdziuba/dev/menu/Main.java
@@ -1,7 +1,7 @@
 package rdziuba.dev.menu;
 
-import java.util.List;
-import java.util.Optional;
+import java.math.BigDecimal;
+import java.util.*;
 
 public class Main {
     static void main(String[] args) {
@@ -30,5 +30,27 @@ public class Main {
         responseText = item1.map(MenuItem::toString).orElse("Item não encontrado");
         System.out.println(responseText);
 
+        Set<MenuItem.MenuCategory> itemsOnSale = EnumSet.of(MenuItem.MenuCategory.DESSERT, MenuItem.MenuCategory.APPETIZER);
+        itemsOnSale.add(MenuItem.MenuCategory.MAIN_COURSE);
+        itemsOnSale.forEach(System.out::println);
+
+        System.out.println("-----");
+
+        Map<MenuItem.MenuCategory, String> sales = new EnumMap<>(MenuItem.MenuCategory.class);
+        sales.put(MenuItem.MenuCategory.DESSERT, "O doce perfeito para você!");
+        sales.put(MenuItem.MenuCategory.APPETIZER, "Começe sua refeição com um toque de sabor!");
+        System.out.println(sales);
+
+        System.out.println(database.getMenuItems());
+        System.out.println("-------");
+        long idForRemove = 1L;
+        boolean isRemoved = database.removeById(idForRemove);
+        System.out.println(isRemoved ? "Removido com sucesso" : "Não foi removido");
+        System.out.println(database.getMenuItems());
+        // Weakhashmap has weak relation that can be clear with the gc
+        database.updatePriceById(2L, new BigDecimal("3.99"));
+        database.updatePriceById(2L, new BigDecimal("3.50"));
+        database.updatePriceById(2L, new BigDecimal("4.99"));
+        database.printPriceHistory();
     }
 }

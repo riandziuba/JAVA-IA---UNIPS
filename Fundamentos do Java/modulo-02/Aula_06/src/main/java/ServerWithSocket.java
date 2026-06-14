736699a6ccba93c727a42c4deae6eb3f18dcb386
riandziuba <riandziuba@gmail.com>
2026-06-14 13:35:37 -0300

    feat: server and client with socket (low level)

---
+++ Fundamentos do Java/modulo-02/Aula_06/src/main/java/ServerWithSocket.java
@@ -0,0 +1,39 @@
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class ServerWithSocket {
+    static void main(String[] args) throws Exception {
+        while (true) {
+            try (ServerSocket serverSocket = new ServerSocket(8000)) {
+                System.out.println("Server started");
+
+                try (Socket clientSocket = serverSocket.accept()) {
+                    InputStream clientIS = clientSocket.getInputStream();
+                    StringBuilder stringBuilder = new StringBuilder();
+                    int data;
+                    do {
+                        data = clientIS.read();
+                        stringBuilder.append((char) data);
+                    } while (clientIS.available() > 0);
+                    String request = stringBuilder.toString();
+                    System.out.println(request);
+
+                    String json = Files.readString(Path.of("items.json"));
+
+                    OutputStream clientOS = clientSocket.getOutputStream();
+                    PrintStream clientOut = new PrintStream(clientOS);
+
+                    clientOut.println("HTTP/1.1 200 OK");
+                    clientOut.println("Content-type: application/json; charset=UTF-8");
+                    clientOut.println();
+                    clientOut.println(json);
+                }
+            }
+        }
+    }
+}

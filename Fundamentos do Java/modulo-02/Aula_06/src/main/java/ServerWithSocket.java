047c1d665b4e0f78b230f8ebdc90443f4ce4249f
riandziuba <riandziuba@gmail.com>
2026-06-16 22:21:19 -0300

    feat: some modification on request handle

--- Fundamentos do Java/modulo-02/Aula_06/src/main/java/ServerWithSocket.java
+++ Fundamentos do Java/modulo-02/Aula_06/src/main/java/ServerWithSocket.java
@@ -34,43 +34,70 @@ public class ServerWithSocket {
                 stringBuilder.append((char) data);
             } while (clientIS.available() > 0);
             String request = stringBuilder.toString();
-            String firstLine = request.substring(0, request.indexOf("\r\n"));
-            String body = request.substring(request.indexOf("\r\n\r\n"));
+            String[] requestChunks = request.split("\\r\\n\\r\\n");
+            String requestLineAndHeaders = requestChunks[0];
+            String[] requestLineAndHeadersChunk = requestLineAndHeaders.split("\\r\\n");
+            String requestLine = requestLineAndHeadersChunk[0];
+            String[] requestLineChunks = requestLine.split(" ");
+
+            String method = requestLineChunks[0];
+            String requestURI = requestLineChunks[1];
             System.out.println(request);
 
-            System.out.println("body " + body);
             Thread.sleep(250);
             String response = "";
             Path path = Path.of("items.json");
             String json = Files.readString(path);
-            switch (firstLine) {
-                case "GET /items HTTP/1.1":
-                    response = json;
+            String requestLineReturn = "";
+            switch (method) {
+                case "GET":
+                    switch (requestURI) {
+                        case "/items":
+                            response = json;
+                            break;
+                        case "/items/total":
+                            int total = new JSONArray(json).length();
+                            response = new JSONObject().put("total", total).toString();
+                            break;
+                    }
+                    requestLineReturn = "HTTP/1.1 200 OK";
                 break;
-                case "GET /items/total HTTP/1.1":
-                    int total = new JSONArray(json).length();
-                    response = new JSONObject().put("total", total).toString();
-                    break;
-                case "POST /items HTTP/1.1":
-                    try {
-                        JSONObject itemJson = new JSONObject(body);
-                        JSONArray items = new JSONArray(json);
-                        items.put(itemJson);
-                        Files.writeString(path, items.toString());
-                        response = new JSONObject().put("message", "Item cadastrado com sucesso").toString();
-                    } catch (Exception e) {
-                        response = new JSONObject().put("message", "Item não cadastrado, tente novamente mais tarde").toString();
+                case "POST":
+                    if (requestChunks.length == 1) {
+                        requestLineReturn = "HTTP/1.1 400 Bad Request";
+                        response = new JSONObject().put("message", "Item não cadastrado, não há dados").toString();
+                        break;
+                    }
+                    String body = requestChunks[1];
+                    switch (requestURI) {
+                        case "/items":
+                            try {
+                                JSONObject itemJson = new JSONObject(body);
+                                JSONArray items = new JSONArray(json);
+                                items.put(itemJson);
+                                Files.writeString(path, items.toString());
+                                response = new JSONObject().put("message", "Item cadastrado com sucesso").toString();
+                                requestLineReturn = "HTTP/1.1 201 Created";
+                            } catch (Exception e) {
+                                response = new JSONObject().put("message", "Item não cadastrado, tente novamente mais tarde").toString();
+                                requestLineReturn = "HTTP/1.1 500 Internal Server Error";
+                            }
+                            break;
                     }
             }
 
 
             OutputStream clientOS = clientSocket.getOutputStream();
             PrintStream clientOut = new PrintStream(clientOS);
-
-            clientOut.println("HTTP/1.1 200 OK");
-            clientOut.println("Content-type: application/json; charset=UTF-8");
-            clientOut.println();
-            clientOut.println(response);
+            if (response.isEmpty()) {
+                clientOut.println("HTTP/1.1 404 Not Found");
+                clientOut.println();
+            } else {
+                clientOut.println(requestLineReturn);
+                clientOut.println("Content-type: application/json; charset=UTF-8");
+                clientOut.println();
+                clientOut.println(response);
+            }
         } catch (Exception e) {
             throw new RuntimeException(e);
         }

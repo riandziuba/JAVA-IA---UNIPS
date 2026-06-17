afc8f2c10a82589f91056bf5fd8a5fa88e797567
riandziuba <riandziuba@gmail.com>
2026-06-16 21:38:58 -0300

    feat: homework of three request treatment

--- Fundamentos do Java/modulo-02/Aula_06/src/main/java/ServerWithSocket.java
+++ Fundamentos do Java/modulo-02/Aula_06/src/main/java/ServerWithSocket.java
@@ -1,21 +1,25 @@
-import java.io.IOException;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
-import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ServerWithSocket {
     static void main(String[] args) throws Exception {
-        try (ServerSocket serverSocket = new ServerSocket(8000)) {
-            System.out.println("Server started");
-            while (true) {
-                Socket clientSocket = serverSocket.accept();
-                Thread request = new Thread(() ->  processRequest(clientSocket));
-                request.start();
+        try (ExecutorService executorService = Executors.newFixedThreadPool(50)) {
+            try (ServerSocket serverSocket = new ServerSocket(8000)) {
+                System.out.println("Server started");
+                while (true) {
+                    Socket clientSocket = serverSocket.accept();
+                    executorService.execute(() -> processRequest(clientSocket));
+                }
             }
         }
     }
@@ -30,11 +34,35 @@ public class ServerWithSocket {
                 stringBuilder.append((char) data);
             } while (clientIS.available() > 0);
             String request = stringBuilder.toString();
+            String firstLine = request.substring(0, request.indexOf("\r\n"));
+            String body = request.substring(request.indexOf("\r\n\r\n"));
             System.out.println(request);
 
+            System.out.println("body " + body);
             Thread.sleep(250);
+            String response = "";
+            Path path = Path.of("items.json");
+            String json = Files.readString(path);
+            switch (firstLine) {
+                case "GET /items HTTP/1.1":
+                    response = json;
+                break;
+                case "GET /items/total HTTP/1.1":
+                    int total = new JSONArray(json).length();
+                    response = new JSONObject().put("total", total).toString();
+                    break;
+                case "POST /items HTTP/1.1":
+                    try {
+                        JSONObject itemJson = new JSONObject(body);
+                        JSONArray items = new JSONArray(json);
+                        items.put(itemJson);
+                        Files.writeString(path, items.toString());
+                        response = new JSONObject().put("message", "Item cadastrado com sucesso").toString();
+                    } catch (Exception e) {
+                        response = new JSONObject().put("message", "Item não cadastrado, tente novamente mais tarde").toString();
+                    }
+            }
 
-            String json = Files.readString(Path.of("items.json"));
 
             OutputStream clientOS = clientSocket.getOutputStream();
             PrintStream clientOut = new PrintStream(clientOS);
@@ -42,7 +70,7 @@ public class ServerWithSocket {
             clientOut.println("HTTP/1.1 200 OK");
             clientOut.println("Content-type: application/json; charset=UTF-8");
             clientOut.println();
-            clientOut.println(json);
+            clientOut.println(response);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }

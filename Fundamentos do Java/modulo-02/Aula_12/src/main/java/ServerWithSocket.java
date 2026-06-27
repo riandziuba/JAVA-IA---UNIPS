d47d6756ce24737c20dc1466e613fda7b4ac7c22
riandziuba <riandziuba@gmail.com>
2026-06-27 18:03:49 -0300

    feat: logging implementation

--- Fundamentos do Java/modulo-02/Aula_12/src/main/java/ServerWithSocket.java
+++ Fundamentos do Java/modulo-02/Aula_12/src/main/java/ServerWithSocket.java
@@ -13,14 +13,17 @@ import java.net.Socket;
 import java.util.Optional;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ServerWithSocket {
+    private static final Logger logger = Logger.getLogger(ServerWithSocket.class.getName());
     private static final IDatabase DATABASE = new MySqlDatabase();
     static void main(String[] args) throws Exception {
         ExecutorService executorService = Executors.newFixedThreadPool(50);
         try {
             try (ServerSocket serverSocket = new ServerSocket(8000)) {
-                System.out.println("Server started");
+                logger.info("Server started");
                 while (true) {
                     Socket clientSocket = serverSocket.accept();
                     executorService.execute(() -> processRequest(clientSocket));
@@ -50,58 +53,64 @@ public class ServerWithSocket {
 
             String method = requestLineChunks[0];
             String requestURI = requestLineChunks[1];
-            System.out.println(request);
-
+            String httpVersion = requestLineChunks[2];
+            logger.finest(request);
+            logger.fine(() -> "Metodo: " + method);
+            logger.fine(() -> "RequestURI: " + requestURI);
+            logger.fine(() -> "Http version: " + httpVersion);
             Thread.sleep(250);
             String response = "";
             String requestLineReturn = "";
             JSONObject jsonBody = new JSONObject(body);
-            switch (method) {
-                case "GET":
-                    switch (requestURI) {
-                        case "/items":
-                            response = DATABASE.menuItemsToJson().toString();
-                            break;
-                        case "/items/total":
-                            int total = DATABASE.totalOfItems();
-                            response = new JSONObject().put("total", total).toString();
-                            break;
-                        default:
-                            if (requestURI.startsWith("/items/")) {
-                                Long id = Long.parseLong(requestURI.split("/items/")[1]);
-                                Optional<MenuItem> itemOptional = DATABASE.getById(id);
-                                if (itemOptional.isEmpty()) {
-                                    requestLineReturn = "HTTP/1.1 404 Not Found";
-                                    response = new JSONObject().put("message", "Item id (" + id + ") não existe").toString();
-                                    break;
+            OutputStream clientOS = clientSocket.getOutputStream();
+            PrintStream clientOut = new PrintStream(clientOS);
+            try {
+                switch (method) {
+                    case "GET":
+                        switch (requestURI) {
+                            case "/items":
+                                response = DATABASE.menuItemsToJson().toString();
+                                break;
+                            case "/items/total":
+                                int total = DATABASE.totalOfItems();
+                                response = new JSONObject().put("total", total).toString();
+                                break;
+                            default:
+                                if (requestURI.startsWith("/items/")) {
+                                    Long id = Long.parseLong(requestURI.split("/items/")[1]);
+                                    Optional<MenuItem> itemOptional = DATABASE.getById(id);
+                                    if (itemOptional.isEmpty()) {
+                                        requestLineReturn = "HTTP/1.1 404 Not Found";
+                                        response = new JSONObject().put("message", "Item id (" + id + ") não existe").toString();
+                                        break;
+                                    }
+                                    response = itemOptional.get().toJson().toString();
                                 }
-                                response = itemOptional.get().toJson().toString();
-                            }
 
-                    }
-                    if (requestLineReturn.isEmpty()) requestLineReturn = "HTTP/1.1 200 OK";
-                break;
-                case "POST":
-                    if (requestChunks.length == 1) {
-                        requestLineReturn = "HTTP/1.1 400 Bad Request";
-                        response = new JSONObject().put("message", "Item não cadastrado, não há dados").toString();
+                        }
+                        if (requestLineReturn.isEmpty()) requestLineReturn = "HTTP/1.1 200 OK";
                         break;
-                    }
-                    switch (requestURI) {
-                        case "/items":
-                            try {
-                                JSONObject itemJson = jsonBody;
-                                DATABASE.add(MenuItem.fromJson(itemJson));
-                                response = new JSONObject().put("message", "Item cadastrado com sucesso").toString();
-                                requestLineReturn = "HTTP/1.1 201 Created";
-                            } catch (Exception e) {
-                                response = new JSONObject().put("message", "Item não cadastrado, tente novamente mais tarde. Erro:" + e.getMessage()).toString();
-                                requestLineReturn = "HTTP/1.1 500 Internal Server Error";
-                            }
+                    case "POST":
+                        if (requestChunks.length == 1) {
+                            requestLineReturn = "HTTP/1.1 400 Bad Request";
+                            response = new JSONObject().put("message", "Item não cadastrado, não há dados").toString();
                             break;
-                    }
-                case "DELETE":
-                    if (requestURI.startsWith("/items/")) {
+                        }
+                        switch (requestURI) {
+                            case "/items":
+                                try {
+                                    JSONObject itemJson = jsonBody;
+                                    DATABASE.add(MenuItem.fromJson(itemJson));
+                                    response = new JSONObject().put("message", "Item cadastrado com sucesso").toString();
+                                    requestLineReturn = "HTTP/1.1 201 Created";
+                                } catch (Exception e) {
+                                    response = new JSONObject().put("message", "Item não cadastrado, tente novamente mais tarde. Erro:" + e.getMessage()).toString();
+                                    requestLineReturn = "HTTP/1.1 500 Internal Server Error";
+                                }
+                                break;
+                        }
+                    case "DELETE":
+                        if (requestURI.startsWith("/items/")) {
                             Long id = Long.parseLong(requestURI.split("/items/")[1]);
                             boolean deleted = DATABASE.removeById(id);
                             if (deleted) {
@@ -118,33 +127,34 @@ public class ServerWithSocket {
                             response = new JSONObject("message", "O item não pode ser deletado").toString();
                             requestLineReturn = "HTTP/1.1 400 Bad Request";
                         }
-                    break;
-                case "PATCH":
-                    if (requestURI.startsWith("/items/")) {
-                        Long id = Long.parseLong(requestURI.split("/items/")[1]);
-                        BigDecimal newPrice = jsonBody.getBigDecimal("newPrice");
-                        boolean updated = DATABASE.updatePriceById(id, newPrice);
-                        if (updated) {
-                            response = "";
-                            requestLineReturn = "HTTP/1.1 204 No Content";
-                            break;
+                        break;
+                    case "PATCH":
+                        if (requestURI.startsWith("/items/")) {
+                            Long id = Long.parseLong(requestURI.split("/items/")[1]);
+                            BigDecimal newPrice = jsonBody.getBigDecimal("newPrice");
+                            boolean updated = DATABASE.updatePriceById(id, newPrice);
+                            if (updated) {
+                                response = "";
+                                requestLineReturn = "HTTP/1.1 204 No Content";
+                                break;
+                            }
+                            Optional<MenuItem> itemOptional = DATABASE.getById(id);
+                            if (itemOptional.isEmpty()) {
+                                response = "";
+                                requestLineReturn = "HTTP/1.1 404 Not Found";
+                                break;
+                            }
+                            response = new JSONObject("message", "O preço do item não pode ser alterado").toString();
+                            requestLineReturn = "HTTP/1.1 400 Bad Request";
                         }
-                        Optional<MenuItem> itemOptional = DATABASE.getById(id);
-                        if (itemOptional.isEmpty()) {
-                            response = "";
-                            requestLineReturn = "HTTP/1.1 404 Not Found";
-                            break;
-                        }
-                        response = new JSONObject("message", "O preço do item não pode ser alterado").toString();
-                        requestLineReturn = "HTTP/1.1 400 Bad Request";
-                    }
-
-
+                }
+            } catch (Exception e) {
+                clientOut.println("HTTP/1.1 500 Internal Server Error");
+                clientOut.println();
+                clientOut.println(e.getMessage());
+                logger.log(Level.SEVERE, e, () -> "Erro ao processar metodo " + method + " " + requestURI);
             }
 
-
-            OutputStream clientOS = clientSocket.getOutputStream();
-            PrintStream clientOut = new PrintStream(clientOS);
             if (response.isEmpty() && requestLineReturn.isEmpty()) {
                 clientOut.println("HTTP/1.1 404 Not Found");
                 clientOut.println();

17f4fc36bbb1f98deaef6fc5925429ab17981dd4
riandziuba <riandziuba@gmail.com>
2026-07-02 21:28:57 -0300

    feat: Circuit Breaker and Health Check

---
+++ Desenvolvimento de aplicações backend com SpringBoot e Quarkus/modulo 01/aula_01/src/main/java/dev/rdziuba/ReadinessCheck.java
@@ -0,0 +1,21 @@
+package dev.rdziuba;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Readiness;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+@Readiness
+public class ReadinessCheck implements HealthCheck {
+
+    @RestClient
+    StarWarsService starWarsService;
+
+    @Override
+    public HealthCheckResponse call() {
+        if (starWarsService.getStarships().contains(StarWarsService.MSG_ERROR)) {
+            return HealthCheckResponse.down("ERROR");
+        }
+        return HealthCheckResponse.up("OK");
+    }
+}
